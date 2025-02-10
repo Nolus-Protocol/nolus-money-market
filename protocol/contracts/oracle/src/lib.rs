ce53fbb6c7e1e1fb2fcf12f4d55ab0928cc4d8f5
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2025-02-10 15:54:45 +0200

    temp ~ 4

--- protocol/contracts/oracle/src/lib.rs
+++ protocol/contracts/oracle/src/lib.rs
@@ -8,7 +8,7 @@ pub mod result;
 #[cfg(feature = "contract")]
 pub mod state;
 pub mod stub;
-#[cfg(all(feature = "stub_swap_testing", test))]
+#[cfg(feature = "stub_swap_testing")]
 pub mod test_tree;
 #[cfg(all(feature = "internal.test.contract", test))]
 mod tests;
